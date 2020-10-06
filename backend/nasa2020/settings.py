5a446d2ff7a2f188c0433cc7293fb479707f923c
MaChimal <mariochimal00aa@gmail.com>
2020-10-05 23:52:24 -0500

    secret_key

--- backend/nasa2020/settings.py
+++ backend/nasa2020/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ze@^!-32of=qg-zvhvl62=w_g50de61yv9c@cnbl=so_d3=yo*'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
